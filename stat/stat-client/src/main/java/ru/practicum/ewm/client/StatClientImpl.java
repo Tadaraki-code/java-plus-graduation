ee7336a098cb0f1e6645de5c7a249c0d9558f299
Sergey Andreev <siarhe1.andreeff@yandex.kz>
2025-07-18 15:02:06 +0300

    Изменение структуры проекта, добавление config-server, discovery-server, gateway-server.

--- stat/stat-client/src/main/java/ru/practicum/ewm/client/StatClientImpl.java
+++ stat/stat-client/src/main/java/ru/practicum/ewm/client/StatClientImpl.java
@@ -2,6 +2,9 @@ package ru.practicum.ewm.client;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -21,21 +24,26 @@ import java.util.List;
 public class StatClientImpl implements StatClient {
 
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    private final RestClient restClient;
+    private final DiscoveryClient discoveryClient;
+    private final String statServerId;
+    private  final RestClient restClient;
     private final ObjectMapper mapper;
 
-    public StatClientImpl(RestClient restClient, ObjectMapper mapper) {
-        this.restClient = restClient;
+    public StatClientImpl(ObjectMapper mapper, DiscoveryClient discoveryClient,
+                          @Value("${stat-service.stat-server-id}") String statServerId) {
         this.mapper = mapper;
+        this.restClient = RestClient.builder().build();
+        this.discoveryClient = discoveryClient;
+        this.statServerId = statServerId;
     }
 
     public void hit(StatHitDto statHitDto) {
-
         String jsonBody;
+        String statServerUri = getStatServerId();
         try {
             jsonBody = mapper.writeValueAsString(statHitDto);
             restClient.post()
-                    .uri("/hit")
+                    .uri(statServerUri + "/hit")
                     .contentType(MediaType.APPLICATION_JSON)
                     .body(jsonBody)
                     .retrieve()
@@ -49,6 +57,8 @@ public class StatClientImpl implements StatClient {
     @Override
     public List<StatViewDto> getStat(LocalDateTime start, LocalDateTime end,
                                      List<String> uris, Boolean unique) {
+        String statServerUri = getStatServerId();
+        System.out.println(statServerUri);
         try {
             UriComponentsBuilder builder = UriComponentsBuilder.newInstance()
                     .path("/stats")
@@ -66,7 +76,7 @@ public class StatClientImpl implements StatClient {
             String uri = builder.build().toUriString();
 
             return restClient.get()
-                    .uri(uri)
+                    .uri(statServerUri + uri)
                     .retrieve()
                     .body(new ParameterizedTypeReference<List<StatViewDto>>() {
                     });
@@ -74,5 +84,13 @@ public class StatClientImpl implements StatClient {
             log.error("Ошибка при запросе на получение статистики", e);
             return new ArrayList<>();
         }
+    }
+
+    private String getStatServerId() {
+        List<ServiceInstance> instances = discoveryClient.getInstances(statServerId);
+        if (instances.isEmpty()) {
+            throw new IllegalStateException("Адрес сервера статистики не найден");
+        }
+        return instances.getFirst().getUri().toString();
     }
 }
